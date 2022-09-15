a8ea83515c7b59020a1b47c4295c8b00f21b7c13
nestor <nestordepablos14@gmail.com>
2022-09-14 23:35:51 -0300

    clase9-generando eventos

--- clase9-agregando-eventos/js/script2.js
+++ clase9-agregando-eventos/js/script2.js
@@ -1,52 +1,15 @@
 
+function initCPU() {
+
+    const miPosicion = document.getElementById('procesadorCPU');
 
     let arregloCPU = [
         { id: 1, nombre: "Intel i3", precio: 55000 },
         { id: 2, nombre: "Intel i5", precio: 65000 },
         { id: 3, nombre: "Intel i7", precio: 75000 },
       ];
-
-    let arregloMonitor = [
-        { id: 1, nombre: "Monitor Samsung 22 inch", precio: 63000 },
-        { id: 2, nombre: "Monitor LG 27 inch", precio: 75000 },
-        { id: 3, nombre: "Monitor Itachi 27 inch", precio: 61000 },
-      ];
-
-    let arregloDiscoDuro = [
-        { id: 1, nombre: "WD 500 GB", precio: 15000 },
-        { id: 2, nombre: "Samsung 1TB", precio: 30000 },
-        { id: 3, nombre: "Adata 2TB", precio: 38000 },
-      ];
-
-    let arregloTarjeta = [
-        { id: 1, nombre: "Mother Board Intel", precio: 45000 },
-        { id: 2, nombre: "Mother Board Asus", precio: 65000 },
-        { id: 3, nombre: "Mother Board Gigabit", precio: 77000 },
-      ];
-
-    let arregloMemoriRam = [
-        { id: 1, nombre: "ADATA 8 GB DDR3", precio: 16000 },
-        { id: 2, nombre: "Samsung 8GB DDR4", precio: 18000 },
-        { id: 3, nombre: "Kingston 16 DDR4", precio: 28000 },
-      ];
       
-    let arregloPowerSupply = [
-        { id: 1, nombre: "MSI MPG A850GF 850 Watt", precio: 49000 },
-        { id: 2, nombre: "Corsair CS Series 750 Watt", precio: 43000 },
-        { id: 3, nombre: "EVGA ATX 12V 600 Watt", precio: 38500 },
-      ];
-
-
-
-function initCPU() {
-
-    const miPosicion = document.getElementById('procesadorCPU');
-
-    arregloCPU = [
-        { id: 1, nombre: "Intel i3", precio: 55000 },
-        { id: 2, nombre: "Intel i5", precio: 65000 },
-        { id: 3, nombre: "Intel i7", precio: 75000 },
-      ];
+ 
  
     let select = createOptionsCpu(arregloCPU);
 
@@ -59,11 +22,13 @@ function initMonitor() {
 
     const miPosicion = document.getElementById('monitorVisor');
 
-    arregloMonitor = [
+    let arregloMonitor = [
         { id: 1, nombre: "Monitor Samsung 22 inch", precio: 63000 },
         { id: 2, nombre: "Monitor LG 27 inch", precio: 75000 },
         { id: 3, nombre: "Monitor Itachi 27 inch", precio: 61000 },
       ];
+      
+ 
  
     let select = createOptionsMonitor(arregloMonitor);
 
@@ -75,11 +40,14 @@ function initDisco() {
 
     const miPosicion = document.getElementById('hardDisk');
 
-    arregloDiscoDuro = [
+    let arregloDiscoDuro = [
         { id: 1, nombre: "WD 500 GB", precio: 15000 },
         { id: 2, nombre: "Samsung 1TB", precio: 30000 },
         { id: 3, nombre: "Adata 2TB", precio: 38000 },
       ];
+      
+      
+ 
  
     let select = createOptionsDisco(arregloDiscoDuro);
 
@@ -91,12 +59,16 @@ function initTarjeta() {
 
     const miPosicion = document.getElementById('motherBoard');
 
-    arregloTarjeta = [
+    let arregloTarjeta = [
         { id: 1, nombre: "Mother Board Intel", precio: 45000 },
         { id: 2, nombre: "Mother Board Asus", precio: 65000 },
         { id: 3, nombre: "Mother Board Gigabit", precio: 77000 },
       ];
       
+      
+      
+ 
+ 
     let select = createOptionsTarjeta(arregloTarjeta);
 
     miPosicion.appendChild(select);
@@ -108,12 +80,15 @@ function initRam() {
 
     const miPosicion = document.getElementById('ramMemory');
 
-    arregloMemoriRam = [
+    let arregloMemoriRam = [
         { id: 1, nombre: "ADATA 8 GB DDR3", precio: 16000 },
         { id: 2, nombre: "Samsung 8GB DDR4", precio: 18000 },
         { id: 3, nombre: "Kingston 16 DDR4", precio: 28000 },
       ];
       
+      
+ 
+ 
     let select = createOptionsRam(arregloMemoriRam);
 
     miPosicion.appendChild(select);
@@ -124,17 +99,21 @@ function initPower() {
 
     const miPosicion = document.getElementById('powerSupply');
 
-    arregloPowerSupply = [
+    let arregloPowerSupply = [
         { id: 1, nombre: "MSI MPG A850GF 850 Watt", precio: 49000 },
         { id: 2, nombre: "Corsair CS Series 750 Watt", precio: 43000 },
         { id: 3, nombre: "EVGA ATX 12V 600 Watt", precio: 38500 },
       ];
       
+      
+ 
+ 
     let select = createOptionsPower(arregloPowerSupply);
 
     miPosicion.appendChild(select);
  
 }
+ 
  
 
 function createOptionsCpu(options) {
@@ -282,7 +261,6 @@ function createOptionsPower(options) {
     return select;
 }
 
-//Llamado de las funciones para carga los elemento que se usara en el SELECT
 
 initCPU();
 initMonitor();
@@ -290,6 +268,9 @@ initDisco();
 initTarjeta();
 initRam();
 initPower();
+
+
+
 
 
 
@@ -361,132 +342,3 @@ function CalcularTotal() {
   console.log("subTotal calculado: " + subTotal.value);
   total.value = (parseInt(subTotal.value) + parseInt(subTotal.value*0.21));
 }
-
-
-
-// INTERACTUAL CON EL HTML Y AGREGAR CARD PARA EJECUTAR EVENTOS DE AGREGAR PRODUCTOS AL CARD
-
-
-arregloCPU.forEach( item => {
-
-    let contentCard = document.getElementById('contentCPU')
-    let divCpu = document.createElement('div');
-    divCpu.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divCpu.className = 'col-md-4 card-product';
-    
-    contentCard.append(divCpu);
-})
-
-
-arregloMonitor.forEach( item => {
-
-    let contentCard = document.getElementById('contentMonitor');
-    let divMonitor = document.createElement('div');
-    divMonitor.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divMonitor.className = 'col-md-4 card-product';
-    
-    contentCard.append(divMonitor);
-})
-
-
-arregloDiscoDuro.forEach( item => {
-
-    let contentCard = document.getElementById('contentHardDisk');
-    let divDisco = document.createElement('div');
-    divDisco.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divDisco.className = 'col-md-4 card-product';
-    
-    contentCard.append(divDisco);
-})
-
-
-arregloTarjeta.forEach( item => {
-
-    let contentCard = document.getElementById('contentMotherBoard');
-    let divTarjeta = document.createElement('div');
-    divTarjeta.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divTarjeta.className = 'col-md-4 card-product';
-    
-    contentCard.append(divTarjeta);
-})
-
-
-arregloMemoriRam.forEach( item => {
-
-    let contentCard = document.getElementById('contentMemoryRam');
-    let divRam = document.createElement('div');
-    divRam.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divRam.className = 'col-md-4 card-product';
-    
-    contentCard.append(divRam);
-})
-
-arregloPowerSupply.forEach( item => {
-
-    let contentCard = document.getElementById('contentPowerSuply');
-    let divPower = document.createElement('div');
-    divPower.innerHTML = `
-    <div class="card">
-        <img src="https://placeimg.com/640/480/tech" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${item.nombre}</h4>
-          <h3>${[item.precio]}</h3>
-          <p class="card-text">descripcion producto...</p>
-          <a href="#" class="btn btn-primary">Agregar</a>
-        </div>
-      </div>
-    `;
-    divPower.className = 'col-md-4 card-product';
-    
-    contentCard.append(divPower);
-})
